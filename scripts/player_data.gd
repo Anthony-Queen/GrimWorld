c8449d0be05de2d9575822f82e30433356b73df4
leasx111 <leandrofiorini21@gmail.com>
2026-04-09 12:58:45 +0100

    prepare main, seettings, and load save scenes

---
+++ scripts/player_data.gd
@@ -0,0 +1,15 @@
+extends Resource
+
+class_name PlayerData
+
+@export var max_health : int = 100
+@export var health : int = 100
+@export var max_mana : int = 100
+@export var mana : int = 100
+
+@export var damage : int = 5
+
+@export var resistance : int = 0
+@export var attack : int = 0
+@export var wisdom : int = 0
+@export var luck : int = 0
--- scripts/save_data.gd
+++ scripts/save_data.gd
@@ -1,1 +1,3 @@
 extends Node
+
+@onready var player_data : PlayerData = PlayerData.new()
