219c05893039cb98c6ef64864384f3498ef6d891
leasx111 <leandrofiorini21@gmail.com>
2026-04-10 23:47:32 +0200

    fix choice scene

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -2,10 +2,12 @@ extends Node
 
 
 @warning_ignore_start("unused_signal")
+
 # Dialogue signals
 signal choice1
 signal choice2
 signal choiceScene
+
 @warning_ignore_restore("unused_signal")
 
 var isChoiceBeingMade: bool = false
--- scripts/UI/DialogueChoices.gd
+++ scripts/UI/DialogueChoices.gd
@@ -1,12 +1,15 @@
 extends Control
 
-func _on_button1_pressed() -> void:
+func _on_button_pressed() -> void:
 	print("Choice1")
 	Globals.choice1.emit()
 	Globals.isChoiceBeingMade = false
 
 
-func _on_button2_pressed() -> void:
+func _on_button_2_pressed() -> void:
+	
 	print("Choice2")
+	
 	Globals.choice2.emit()
+	
 	Globals.isChoiceBeingMade = false
